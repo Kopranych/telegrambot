13d29bd26aa86d7c4a64b60b613b9a9d22c4476c
Копранов Илья <i_kopranov@smarteng.ru>
2018-07-13 12:28:48 +0300

    initial commit

---
+++ src/main/java/bot/MyBot.java
@@ -0,0 +1,58 @@
+package bot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.logging.Level;
+
+public class MyBot extends TelegramLongPollingBot {
+    //t.me/Kopranychbot
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+//            log.log(Level.SEVERE, "Exception: ", e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return "Kopranychbot";
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return "581606757:AAHPWRm5cv0JTFNWxGcLdLhN2ZLiglj_J7g";
+    }
+}
---
+++ src/main/java/start/Start.java
@@ -0,0 +1,18 @@
+package start;
+
+import bot.MyBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Start {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new MyBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+}
